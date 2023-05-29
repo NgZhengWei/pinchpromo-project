254313863826d312adfe943b85bb676a0ce2cc2b
NgZhengWei <ploxybee@gmail.com>
2023-05-29 18:38:37 +0800

    Added number of claims functionality

--- src/components/Promotions.jsx
+++ src/components/Promotions.jsx
@@ -1,14 +1,23 @@
 import { useEffect, useState } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import { db } from '../firebase';
-import { getDocs, collection, doc, getDoc } from 'firebase/firestore';
-import { Container } from '@chakra-ui/react';
+import {
+  getDocs,
+  collection,
+  doc,
+  getDoc,
+  updateDoc,
+} from 'firebase/firestore';
+import { Container, Text } from '@chakra-ui/react';
 import BigPromotion from './BigPromotion';
 
 const Promotions = () => {
   const { currentUser } = useAuth();
-  const [username, setUsername] = useState('');
+  const [userData, setUserData] = useState({});
   const [bigPromotions, setBigPromotions] = useState([]);
+  // const [timeToNextClaim, setTimeToNextClaim] = useState('');
+  const [claimRemainingMessage, setClaimRemainingMessage] = useState('');
+  const [runningInterval, setRunningInterval] = useState(false);
 
   const bigPromotionsCollectionRef = collection(db, 'bigPromotions');
 
@@ -43,13 +52,14 @@ const Promotions = () => {
 
         if (currentUser) {
           const userData = await getDoc(userRef);
-          setUsername(userData.data().name);
+          setUserData(userData.data());
 
           filteredData = data.docs.map((doc) => ({
             ...doc.data(),
             id: doc.id,
             promotions: userData.data().promotions,
             usedPromotions: userData.data().usedPromotions,
+            claimAvailable: userData.data().claimAvailable,
           }));
         } else {
           filteredData = data.docs.map((doc) => ({
@@ -69,9 +79,114 @@ const Promotions = () => {
     getBigPromotions();
   }, []);
 
+  useEffect(() => {
+    manageClaims();
+  }, [userData]);
+
+  function manageClaims() {
+    // clear existing intervals. This is for when user uses a claim and it causes 2 intervals to exist, resulting in a bug
+    if (runningInterval) {
+      clearInterval(runningInterval);
+      setRunningInterval(false);
+    }
+
+    if (Object.keys(userData).length === 0) {
+      setClaimRemainingMessage('');
+    } else if (userData.claimAvailable === userData.claimCapacity) {
+      // user has all claims possible, not recharging claims
+      setClaimRemainingMessage(
+        `You have ${userData.claimAvailable}/${userData.claimCapacity} claims. Claim promotions from your favourite brands now!`
+      );
+    } else if (userData.claimAvailable < userData.claimCapacity) {
+      // user is regenerating a new claim
+      const interval = window.setInterval(function () {
+        const now = new Date();
+        const diff = new Date(userData.nextClaimTime) - now;
+
+        if (diff <= 0) {
+          incrementClaims();
+          clearInterval(interval);
+        } else {
+          const hours = Math.floor(
+            (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+          );
+          const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+          const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+          let secondsString = String(seconds);
+          if (secondsString.length === 1) {
+            secondsString = '0' + secondsString;
+          }
+          const timeToNextClaim = hours + ':' + minutes + ':' + secondsString;
+          setClaimRemainingMessage(
+            `You have ${userData.claimAvailable}/${userData.claimCapacity} claims. New claim in ${timeToNextClaim}.`
+          );
+        }
+      }, 1000);
+
+      setRunningInterval(interval);
+    }
+  }
+
+  async function incrementClaims() {
+    try {
+      if (userData.claimAvailable + 1 >= userData.claimCapacity) {
+        // if user has reached their claim capacity, increment claim by 1 and set next claim time to empty so they are not regenerating the next claim
+        updateDoc(doc(db, 'users', currentUser.uid), {
+          nextClaimTime: '',
+          claimAvailable: userData.claimAvailable + 1,
+        });
+        setUserData((prevState) => ({
+          ...prevState,
+          nextClaimTime: '',
+          claimAvailable: userData.claimAvailable + 1,
+        }));
+      } else {
+        // user has not reached claim capacity, increment claim and start regenerating the next claim
+        updateDoc(doc(db, 'users', currentUser.uid), {
+          nextClaimTime: new Date(
+            new Date().getTime() + 60 * 60 * 24 * 1000
+          ).toJSON(),
+          claimAvailable: userData.claimAvailable + 1,
+        });
+        setUserData((prevState) => ({
+          ...prevState,
+          nextClaimTime: new Date(
+            new Date().getTime() + 60 * 60 * 24 * 1000
+          ).toJSON(),
+          claimAvailable: userData.claimAvailable + 1,
+        }));
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async function decrementClaims() {
+    try {
+      const currentTime = new Date().getTime();
+      // nextClaimTime will remain as it is, it it's already defined. Else we will set it to be 24hrs from now
+      const newNextClaimTime =
+        userData.nextClaimTime.length === 0
+          ? new Date(currentTime + 60 * 60 * 24 * 1000).toJSON()
+          : userData.nextClaimTime;
+      updateDoc(doc(db, 'users', currentUser.uid), {
+        nextClaimTime: newNextClaimTime,
+        claimAvailable: userData.claimAvailable - 1,
+      });
+      setUserData((prevState) => ({
+        ...prevState,
+        nextClaimTime: newNextClaimTime,
+        claimAvailable: userData.claimAvailable - 1,
+      }));
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return (
     <Container>
-      <div>Welcome {username}!</div>
+      {/* {currentUser && <Text textAlign='center'>Welcome {userData.name}!</Text>} */}
+      {currentUser && <Text textAlign='center'>{claimRemainingMessage}</Text>}
       {bigPromotions.map((promotion) => {
         // console.log(
         //   promotion.store + ': ' + shouldDisplay(promotion.releaseTime)
@@ -82,7 +197,13 @@ const Promotions = () => {
         //   return '';
         // }
 
-        return <BigPromotion promotion={promotion} key={promotion.id} />;
+        return (
+          <BigPromotion
+            promotion={promotion}
+            key={promotion.id}
+            decrementClaims={decrementClaims}
+          />
+        );
       })}
     </Container>
   );
