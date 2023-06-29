fe370654e806e280368b1ae55d671d8b9728872d
SSTengNic <nicholastengshin@hotmail.com>
2023-06-30 00:51:26 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,18 +6,12 @@ import { getStorage } from 'firebase/storage';
 
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyDiRUMBSEQ9l8E7XTxe3xH9H_F_3VE-3Mc",
-
-  authDomain: "pinchpromo-project-testing.firebaseapp.com",
-
-  projectId: "pinchpromo-project-testing",
-
-  storageBucket: "pinchpromo-project-testing.appspot.com",
-
-  messagingSenderId: "796945746262",
-
-  appId: "1:796945746262:web:5f47b532fb253fd42c6ef7"
-
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 export const auth = app.auth();
