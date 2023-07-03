b245ee3ab912272338b75ab117deaef40b22d316
Ng Zheng Wei <zhengwei@lolMachine.local>
2023-07-03 16:24:23 +0800

    removed google analytics

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,14 +1,14 @@
-import { Box } from '@chakra-ui/react';
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Nav from '../components/Nav';
-import Footer from '../components/Footer';
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 const RootLayout = () => {
   return (
     <Box>
       <Nav />
-      <Box minH='82vh' bg='gray.100' pt='15px'>
+      <Box minH="82vh" bg="gray.100" pt="15px">
         <Outlet />
       </Box>
       <Footer />
