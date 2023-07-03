71a9fb1468ebde4d4d56acd866b72aa04669b1c4
XavierTan06 <tangentxavier@gmail.com>
2023-07-03 14:13:07 +0800

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,35 +1,39 @@
 import { ChatIcon, EmailIcon } from '@chakra-ui/icons';
-import { Box, HStack, Heading, Text } from '@chakra-ui/react';
+import { Box, Flex, HStack, Heading, Text } from '@chakra-ui/react';
 
 const Footer = () => {
   return (
-    <Box
-      bg='brandYellow.100'
+    <Flex
       py='25px'
-      pl={{ base: '10px', sm: '15px', md: '40px' }}
+      px={{ base: '10px', sm: '15px', md: '40px' }}
+      backgroundColor='brandYellow.100'
+      justify='space-between'
+      alignItems='center'
     >
-      <Heading
-        as='h3'
-        mb='10px'
-        fontSize={{ base: '18px', sm: '25px', md: '32px' }}
-      >
-        Contact Us
-      </Heading>
-      <Box ml={2}>
-        <HStack>
-          <ChatIcon />
-          <Text fontSize={{ base: '13px', sm: '15px', md: '16px' }}>
-            Telegram: @alestierK
-          </Text>
-        </HStack>
-        <HStack>
-          <EmailIcon />
-          <Text fontSize={{ base: '13px', sm: '15px', md: '16px' }}>
-            alestier@pinchpromo.com
-          </Text>
-        </HStack>
+      <Box>
+        <Heading
+          as='h3'
+          mb='10px'
+          fontSize={{ base: '18px', sm: '25px', md: '32px' }}
+        >
+          Contact Us
+        </Heading>
+        <Box ml={2}>
+          <HStack>
+            <ChatIcon />
+            <Text fontSize={{ base: '13px', sm: '15px', md: '16px' }}>
+              Telegram: @alestierK
+            </Text>
+          </HStack>
+          <HStack>
+            <EmailIcon />
+            <Text fontSize={{ base: '13px', sm: '15px', md: '16px' }}>
+              Email: hello@pinchpromo.com
+            </Text>
+          </HStack>
+        </Box>
       </Box>
-    </Box>
+    </Flex>
   );
 };
 
