f39027fb127a3515dc37900f8a52af970bbde487
Ng Zheng Wei <zhengwei@lolMachine.local>
2023-07-20 21:48:42 +0800

    added link for telegram and email in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,35 +1,39 @@
-import { ChatIcon, EmailIcon } from '@chakra-ui/icons';
-import { Box, Flex, HStack, Heading, Text } from '@chakra-ui/react';
+import { ChatIcon, EmailIcon } from "@chakra-ui/icons";
+import { Box, Flex, HStack, Heading, Link, Text } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
     <Flex
-      py='25px'
-      px={{ base: '10px', sm: '15px', md: '40px' }}
-      backgroundColor='brandYellow.100'
-      justify='space-between'
-      alignItems='center'
+      py="25px"
+      px={{ base: "10px", sm: "15px", md: "40px" }}
+      backgroundColor="brandYellow.100"
+      justify="space-between"
+      alignItems="center"
     >
       <Box>
         <Heading
-          as='h3'
-          mb='10px'
-          fontSize={{ base: '18px', sm: '25px', md: '32px' }}
+          as="h3"
+          mb="10px"
+          fontSize={{ base: "18px", sm: "25px", md: "32px" }}
         >
           Contact Us
         </Heading>
         <Box ml={2}>
           <HStack>
             <ChatIcon />
-            <Text fontSize={{ base: '13px', sm: '15px', md: '16px' }}>
-              Telegram: @alestierK
-            </Text>
+            <Link href="https://t.me/alestierk">
+              <Text fontSize={{ base: "13px", sm: "15px", md: "16px" }}>
+                Telegram: @alestierK
+              </Text>
+            </Link>
           </HStack>
           <HStack>
             <EmailIcon />
-            <Text fontSize={{ base: '13px', sm: '15px', md: '16px' }}>
-              Email: hello@pinchpromo.com
-            </Text>
+            <Link href="mailto:hello@pinchpromo.com">
+              <Text fontSize={{ base: "13px", sm: "15px", md: "16px" }}>
+                Email: hello@pinchpromo.com
+              </Text>
+            </Link>
           </HStack>
         </Box>
       </Box>
