308d87d703ec61ed4d60e9a0c75c3376ac395b2e
Shao Ren <oonshaoren@gmail.com>
2023-07-24 17:45:33 +0800

    Db foundation with firebase query

--- src/components/businessDashboard/Dashboard.jsx
+++ src/components/businessDashboard/Dashboard.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
+// import { Card, Space, Statistic, Table, Typography } from "antd";
+import { Card, CardHeader, CardBody, CardFooter, Text} from '@chakra-ui/react'
+import firebase from "../../firebase"; // Path to your firebase.js file
 import BarChart from "../charts/BarChart"
 import LineChart from "../charts/LineChart"
 import PieChart from "../charts/PieChart"
@@ -12,8 +15,83 @@ import {
   pieChartOptions,
 } from "../variables/charts"
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 const Dashboard = () => {
-  return <div>oiejoiajf</div>;
+  const [orders, setOrders] = useState(0);
+  const [inventory, setInventory] = useState(0);
+  const [customers, setCustomers] = useState(0);
+  const [revenue, setRevenue] = useState(0);
+  const [businessData, setBusinessData] = useState([]);
+  
+  useEffect(() => {
+    sumNumberOfCoupons().then((res) => {
+      setOrders(res);
+    });
+    // sumNumberOfClaimedCoupons().then((res) => {
+    //   setInventory(res);
+    // });
+    // getCustomers().then((res) => {
+    //   setCustomers(res);
+    // });
+
+    // fetchBusinessData().then((data) => {
+    //   setBusinessData(data);
+    // });
+  }, []);
+  
+  return (  
+    <div>
+          <DashboardCard
+            title={"Total Promotions"}
+            value={orders}
+          />
+          {/* <RecentOrders /> */}
+    </div>
+  );
 };
+    
+const sumNumberOfCoupons = () => {
+  const businessRef = firebase.firestore().collection("bigPromotions");
+  return businessRef.get().then((querySnapshot) => {
+    let totalOrders = 0;
+    querySnapshot.forEach((doc) => {
+      const business = doc.data();
+      totalOrders += business.numberOfCoupons;
+    });
+    return totalOrders;
+  });
+};
+
+const DashboardCard = ({ title, value, icon }) => {
+  return (
+    <Card>
+      {/* <Space direction="horizontal"> */}
+        {icon}
+        title={title} value={value} 
+      {/* </Space> */}
+    </Card>
+  );
+};
+
 
 export default Dashboard;
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,26 @@
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import { getFirestore } from 'firebase/firestore';
+// import { getStorage } from 'firebase/storage';
+
+// const app = firebase.initializeApp({
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+// });
+
+// export const auth = app.auth();
+// export const db = getFirestore(app);
+// export const storage = getStorage(app);
+// export default app;
+
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage'; // Import the compat version of Storage
 
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -13,6 +32,6 @@ const app = firebase.initializeApp({
 });
 
 export const auth = app.auth();
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+export const db = app.firestore();
+export const storage = firebase.storage(); // Use the `firebase.storage()` method to access Storage
 export default app;
