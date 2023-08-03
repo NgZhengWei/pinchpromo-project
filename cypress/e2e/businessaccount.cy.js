c2b1b9f8a760a4321e73ecb96d7a82f0a5f59e0a
SSTengNic <nicholastengshin@hotmail.com>
2023-08-03 12:35:28 +0800

    changes made for further testing
    added some cypress tests, updated the current ones

--- cypress/e2e/businessaccount.cy.js
+++ cypress/e2e/businessaccount.cy.js
@@ -34,143 +34,291 @@ describe('Before logging into business account', () => {
     cy.get('#businessLoginPasswordInput').type('HelloWorld')
     cy.get('#businessLoginButton').click()
     cy.url().should('eq', 'http://localhost:3000/dashboard')
-  })
+  })  
 
-  it('Add new promotion, dashboard updates', () => {
-    cy.get('#businessHamburgerIcon').click()
-    cy.get('#businessCreateNewPromoLink').click()
-
-    //Adding first Big Promo------------------------------------------------------------------------
-    cy.get('#bigPromoStoreNameInput').type('Swifties Yeehaw 1')
-
-    cy.get('#bigPromoPromoTitleInput').type('50% off Taylor Swift Tickets! 1')
-
-    cy.get('#bigPromoAboutBusinessInput').type('We give out good deals for Taylor Swift')
-
-    cy.get('#bigPromoSocialLinkInput').type('www.taylortaylor.com/socialmedia')
-
-    cy.get('#bigPromoAboutWebsiteLinkInput').type('www.taylortaylor.com')
-
-    cy.get('#bigPromoDescriptionInput').type('When you claim this and show Taylor Swift, she will give you 50% off!')
-
-    cy.get('#bigPromoTermsAndConditionsInput').type('You must be a true swifty fan')
-
-    cy.get('#bigPromoCodeInput').type('ILOVETAYLOR50')
-
-    cy.get('#bigPromoInitTimeInput').type('2023-08-02T13:00')
-
-    cy.get('#bigPromoReleaseTimeInput').type('2023-08-02T13:00')
-
-    cy.get('#bigPromoEndTimeInput').type('2023-12-05T17:30')
-    cy.get('#bigLogoImageInput').selectFile('BusinessPromoCypress.png')
-    cy.get('#bigPromoPosterimageInput').selectFile('BusinessPosterCypress.png')
-    cy.get('#bigPromoNumCouponsInput').type('10')
-    cy.get('#bigPromoSelectClaim').select('Website')
-    cy.get('#bigPromoAddPromoButton').click()
-    cy.wait(2000)
-    cy.contains('Successfully added big promotion.')
-
-    //Clearing the previous inputs
-    cy.get('#bigPromoStoreNameInput').clear()
-
-    cy.get('#bigPromoPromoTitleInput').clear()
-    cy.get('#bigPromoAboutBusinessInput').clear()
-
-    cy.get('#bigPromoSocialLinkInput').clear()
-
-    cy.get('#bigPromoAboutWebsiteLinkInput').clear()
-
-    cy.get('#bigPromoDescriptionInput').clear()
-
-    cy.get('#bigPromoTermsAndConditionsInput').clear()
-
-    cy.get('#bigPromoCodeInput').clear()
-
-    cy.get('#bigPromoInitTimeInput').clear()
-    cy.get('#bigPromoReleaseTimeInput').clear()
-
-    cy.get('#bigPromoEndTimeInput').clear()
-
-    cy.get('#bigPromoNumCouponsInput').clear()
-
-
-
-    //Adding second Big Promo------------------------------------------------------------------------
-    cy.get('#businessHamburgerIcon').click()
-    cy.get('#businessCreateNewPromoLink').click()
-
+  it('fuzzer testing for big promo', ()=> {
+      var iter = 0
   
-    cy.get('#bigPromoStoreNameInput').type('Swifties Yeehaw 2')
+      function randomize() {
+        const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        let rand = '';
+        const randLength = Math.floor(Math.random() * 10) + 5; // Random user length between 5 and 14 characters
+        for (let i = 0; i < randLength; i++) {
+          const randomIndex = Math.floor(Math.random() * chars.length);
+          rand += chars.charAt(randomIndex);
+        }
+        return rand;
+      }
 
-    cy.get('#bigPromoPromoTitleInput').type('50% off Taylor Swift Tickets! 2')
+      function randomizeDate() {
+        const currentDate = new Date();
+        const futureDate = new Date(currentDate.getTime() + Math.random() * 100000000000); // Random future date within 100000000000 milliseconds (~3 years)
+        return futureDate.toISOString();
+      }
 
-    cy.get('#bigPromoAboutBusinessInput').type('We give out good deals for Taylor Swift')
+      
+    function omitRandomInput() {
+      const inputFields = [
+        'StoreName',
+        'PromoTitle',
+        'AboutBusiness',
+   
+        'PromoCode',
+        'InitTime',
+        'ReleaseTime',
+        'EndTime',
+        'NumOfCoupons'
+        // Add other input field IDs here as needed
+      ];
+      const randomIndex = Math.floor(Math.random() * inputFields.length);
+      const inputToOmit = inputFields[randomIndex];
+      return inputToOmit
+    }
 
-    cy.get('#bigPromoSocialLinkInput').type('www.taylortaylor.com/socialmedia')
+      cy.get('#businessHamburgerIcon').click()
+      cy.get('#businessCreateNewPromoLink').click()
+      
+      while (iter <10) {
+        const omittedInput = omitRandomInput();
 
-    cy.get('#bigPromoAboutWebsiteLinkInput').type('www.taylortaylor.com')
+        if (omittedInput === 'StoreName'){
+          cy.get('#bigPromoPromoTitleInput').type(randomize())
 
-    cy.get('#bigPromoDescriptionInput').type('When you claim this and show Taylor Swift, she will give you 50% off!')
+          cy.get('#bigPromoAboutBusinessInput').type(randomize())
+  
+          cy.get('#bigPromoAboutWebsiteLinkInput').type(randomize())
+  
+          cy.get('#bigPromoSocialLinkInput').type(randomize())
+  
+          cy.get('#bigPromoTermsAndConditionsInput').type(randomize())
+  
+          cy.get('#bigPromoDescriptionInput').type(randomize())
+  
+          cy.get('#bigPromoCodeInput').type(randomize())
+  
+          cy.get('#bigPromoInitTimeInput').type('2023-12-05T17:30')
+  
+          cy.get('#bigPromoReleaseTimeInput').type('2023-12-05T17:30')
+  
+          cy.get('#bigPromoEndTimeInput').type('2023-12-05T17:30')
+  
+          cy.get('#bigLogoImageInput').selectFile('BusinessPromoCypress2.png')
+  
+          cy.get('#bigPromoPosterimageInput').selectFile('BusinessPosterCypress2.png')
+  
+          cy.get('#bigPromoNumCouponsInput').type( Math.floor(Math.random() *100 ))
+  
+          cy.get('#bigPromoSelectClaim').select('Website')
 
-    cy.get('#bigPromoTermsAndConditionsInput').type('You must be a true swifty fan')
+        }
+        else if (omittedInput === 'PromoTitle') {
+          cy.get('#bigPromoStoreNameInput').type(randomize())
+          cy.get('#bigPromoAboutBusinessInput').type(randomize())
+  
+          cy.get('#bigPromoAboutWebsiteLinkInput').type(randomize())
+  
+          cy.get('#bigPromoSocialLinkInput').type(randomize())
+  
+          cy.get('#bigPromoTermsAndConditionsInput').type(randomize())
+  
+          cy.get('#bigPromoDescriptionInput').type(randomize())
+  
+          cy.get('#bigPromoCodeInput').type(randomize())
+  
+          cy.get('#bigPromoInitTimeInput').type('2023-08-02T13:00')
+  
+          cy.get('#bigPromoReleaseTimeInput').type('2023-08-02T13:00')
+  
+          cy.get('#bigPromoEndTimeInput').type('2023-12-05T17:30')
+  
+          cy.get('#bigLogoImageInput').selectFile('BusinessPromoCypress2.png')
+  
+          cy.get('#bigPromoPosterimageInput').selectFile('BusinessPosterCypress2.png')
+  
+          cy.get('#bigPromoNumCouponsInput').type(randomize())
+  
+          cy.get('#bigPromoSelectClaim').select('Website')
+        }
+        else if (omittedInput === 'AboutBusiness') {
+          cy.get('#bigPromoStoreNameInput').type(randomize())
 
-    cy.get('#bigPromoCodeInput').type('ILOVETAYLOR50')
+          cy.get('#bigPromoPromoTitleInput').type(randomize())
 
-    cy.get('#bigPromoInitTimeInput').type('2023-08-02T13:00')
+          cy.get('#bigPromoAboutWebsiteLinkInput').type(randomize())
 
-    cy.get('#bigPromoReleaseTimeInput').type('2023-08-02T13:00')
+          cy.get('#bigPromoSocialLinkInput').type(randomize())
+  
+          cy.get('#bigPromoTermsAndConditionsInput').type(randomize())
+  
+          cy.get('#bigPromoDescriptionInput').type(randomize())
+  
+          cy.get('#bigPromoCodeInput').type(randomize())
+  
+          cy.get('#bigPromoInitTimeInput').type('2023-08-02T13:00')
+  
+          cy.get('#bigPromoReleaseTimeInput').type('2023-08-02T13:00')
+  
+          cy.get('#bigPromoEndTimeInput').type('2023-12-05T17:30')
+  
+          cy.get('#bigLogoImageInput').selectFile('BusinessPromoCypress2.png')
+  
+          cy.get('#bigPromoPosterimageInput').selectFile('BusinessPosterCypress2.png')
+  
+          cy.get('#bigPromoNumCouponsInput').type( Math.floor(Math.random() *100 ))
+  
+          cy.get('#bigPromoSelectClaim').select('Website')
+  
+        }
+        else if (omittedInput === 'InitTime') {
+          cy.get('#bigPromoStoreNameInput').type(randomize())
 
-    cy.get('#bigPromoEndTimeInput').type('2023-12-05T17:30')
-    cy.get('#bigLogoImageInput').selectFile('BusinessPromoCypress2.png')
-    cy.get('#bigPromoPosterimageInput').selectFile('BusinessPosterCypress2.png')
-    cy.get('#bigPromoNumCouponsInput').type('10')
-    cy.get('#bigPromoSelectClaim').select('Website')
-    cy.get('#bigPromoAddPromoButton').click()
-    cy.wait(2000)
-    cy.contains('Successfully added big promotion.')
+          cy.get('#bigPromoPromoTitleInput').type(randomize())
+  
+          cy.get('#bigPromoAboutBusinessInput').type(randomize())
+  
+          cy.get('#bigPromoAboutWebsiteLinkInput').type(randomize())
+  
+          cy.get('#bigPromoSocialLinkInput').type(randomize())
+  
+          cy.get('#bigPromoTermsAndConditionsInput').type(randomize())
+  
+          cy.get('#bigPromoDescriptionInput').type(randomize())
+  
+          cy.get('#bigPromoCodeInput').type(randomize())
 
-    cy.visit('http://localhost:3000/dashboard')
+          cy.get('#bigPromoReleaseTimeInput').type('2023-08-02T13:00')
+  
+          cy.get('#bigPromoEndTimeInput').type('2023-12-05T17:30')
+  
+          cy.get('#bigLogoImageInput').selectFile('BusinessPromoCypress2.png')
+  
+          cy.get('#bigPromoPosterimageInput').selectFile('BusinessPosterCypress2.png')
+  
+          cy.get('#bigPromoNumCouponsInput').type(randomize())
+  
+          cy.get('#bigPromoSelectClaim').select('Website')
+        }
+        else if (omittedInput === 'ReleaseTime'){
+          cy.get('#bigPromoStoreNameInput').type(randomize())
 
-  })
+          cy.get('#bigPromoPromoTitleInput').type(randomize())
+  
+          cy.get('#bigPromoAboutBusinessInput').type(randomize())
+  
+          cy.get('#bigPromoAboutWebsiteLinkInput').type(randomize())
+  
+          cy.get('#bigPromoSocialLinkInput').type(randomize())
+  
+          cy.get('#bigPromoTermsAndConditionsInput').type(randomize())
+  
+          cy.get('#bigPromoDescriptionInput').type(randomize())
+  
+          cy.get('#bigPromoCodeInput').type(randomize())
+  
+          cy.get('#bigPromoInitTimeInput').type('2023-08-02T13:00')
 
-  it.only('Log into user account, see the new promotions', ()=> {
+          cy.get('#bigPromoEndTimeInput').type('2023-12-05T17:30')
+  
+          cy.get('#bigLogoImageInput').selectFile('BusinessPromoCypress2.png')
+  
+          cy.get('#bigPromoPosterimageInput').selectFile('BusinessPosterCypress2.png')
+  
+          cy.get('#bigPromoNumCouponsInput').type( Math.floor(Math.random() *100 ))
+  
+          cy.get('#bigPromoSelectClaim').select('Website')
+        }
+        else if (omittedInput === 'EndTime'){
+          cy.get('#bigPromoStoreNameInput').type(randomize())
 
+          cy.get('#bigPromoPromoTitleInput').type(randomize())
+  
+          cy.get('#bigPromoAboutBusinessInput').type(randomize())
+  
+          cy.get('#bigPromoAboutWebsiteLinkInput').type(randomize())
+  
+          cy.get('#bigPromoSocialLinkInput').type(randomize())
+  
+          cy.get('#bigPromoTermsAndConditionsInput').type(randomize())
+  
+          cy.get('#bigPromoDescriptionInput').type(randomize())
+  
+          cy.get('#bigPromoCodeInput').type(randomize())
+  
+          cy.get('#bigPromoInitTimeInput').type('2023-08-02T13:00')
 
-    // cy.get('#businessHamburgerIcon').click()
-    // cy.get('#businessProfileLink').click()
-    // cy.contains("Logout").click()
+          cy.get('#bigPromoReleaseTimeInput').type('2023-08-02T13:00')
+  
+          cy.get('#bigLogoImageInput').selectFile('BusinessPromoCypress2.png')
+  
+          cy.get('#bigPromoPosterimageInput').selectFile('BusinessPosterCypress2.png')
+  
+          cy.get('#bigPromoNumCouponsInput').type( Math.floor(Math.random() *100 ))
+  
+          cy.get('#bigPromoSelectClaim').select('Website')
+          
+        }
+        else if (omittedInput === 'NumOfCoupons'){
 
-    // cy.visit('http://localhost:3000/login')
-    // cy.contains('Login').click()
-    // cy.get('#emailLogin').type('tester@email.com')
-    // cy.get('#passwordLogin').type('helloworld')
-    // cy.get('#logginInButton').click()
-    // cy.wait(1000)
+          cy.get('#bigPromoStoreNameInput').type(randomize())
 
-    // cy.contains('Swifties Yeehaw 1')
-    // cy.contains('Swifties Yeehaw 2')
-    // cy.contains('Swifties Yeehaw 2').parent().find('button').as('swiftiesButton')
-    // cy.get('@swiftiesButton').click()
-    // cy.url().should('eq', 'http://localhost:3000/mypromotions')
-    // //Logging out to go back into business account
-    // cy.get('#hamburgerIcon').click()
-    // cy.contains('Profile').click()
-    // cy.contains('Logout').click()
+          cy.get('#bigPromoPromoTitleInput').type(randomize())
+  
+          cy.get('#bigPromoAboutBusinessInput').type(randomize())
+  
+          cy.get('#bigPromoAboutWebsiteLinkInput').type(randomize())
+  
+          cy.get('#bigPromoSocialLinkInput').type(randomize())
+  
+          cy.get('#bigPromoTermsAndConditionsInput').type(randomize())
+  
+          cy.get('#bigPromoDescriptionInput').type(randomize())
+  
+          cy.get('#bigPromoCodeInput').type(randomize())
+  
+          cy.get('#bigPromoInitTimeInput').type('2023-08-02T13:00')
 
-    // cy.visit('http://localhost:3000/businesslogin')
+          cy.get('#bigPromoReleaseTimeInput').type('2023-08-02T13:00')
+  
+          cy.get('#bigLogoImageInput').selectFile('BusinessPromoCypress2.png')
+  
+          cy.get('#bigPromoPosterimageInput').selectFile('BusinessPosterCypress2.png')
+  
+          cy.get('#bigPromoSelectClaim').select('Website')
+          
+        }
 
+        cy.get('#bigPromoAddPromoButton').click()
 
-    //Logging into business account to check dashboard
-    cy.get('#businessLoginEmailInput').type('testerbusiness@email.com')
-    cy.get('#businessLoginPasswordInput').type('HelloWorld')
-    cy.get('#businessLoginButton').click()
-    cy.url().should('eq', 'http://localhost:3000/dashboard')
-    cy.contains("Total Promotions").parent().should('have.text','Total Promotions20')
-    cy.contains("Promotions Claimed").parent().should('have.text','Promotions Claimed2')
+        cy.contains('Successfully added big promotion.').should('not.exist');
+        //Clearing
 
-  } )
+        cy.get('#bigPromoStoreNameInput').clear()
 
+        cy.get('#bigPromoPromoTitleInput').clear()
+        cy.get('#bigPromoAboutBusinessInput').clear()
+    
+        cy.get('#bigPromoSocialLinkInput').clear()
+    
+        cy.get('#bigPromoAboutWebsiteLinkInput').clear()
+    
+        cy.get('#bigPromoTermsAndConditionsInput').clear()
 
+        cy.get('#bigPromoDescriptionInput').clear()
+    
+        cy.get('#bigPromoCodeInput').clear()
+    
+        cy.get('#bigPromoInitTimeInput').clear()
+        cy.get('#bigPromoReleaseTimeInput').clear()
+    
+        cy.get('#bigPromoEndTimeInput').clear()
+    
+        cy.get('#bigPromoNumCouponsInput').clear()
+    
+        //should not go through, how to check
+        //insert checker here 
+        iter+=1
+      }
+    })
 })
 
 
---
+++ cypress/e2e/pinch_promo_test.cy.js
@@ -0,0 +1,327 @@
+/* eslint-disable no-undef */
+//README: Before starting the test, remember to delete the current user account
+describe('Before Logging in Test', function() {
+
+  beforeEach(function() {
+    cy.visit('http://localhost:3000')
+    cy.get('#signUpNavButton')
+    cy.get('#loginNavButton')
+
+  })
+
+  it('Front Page and Footer Information', function() {
+    cy.contains('PinchPromo')
+    cy.contains('Gomgom')
+    cy.contains('Promotions Available')
+    cy.contains('Contact Us')
+    cy.contains('Telegram: @alestierK')
+  })
+  
+  it('"How to?" drop down', function() {
+    cy.get('#dropDownInfo').click()
+    cy.contains('New exclusive promos are released daily for you to maximise your dollar.')
+    cy.get('#signUpLink')
+    cy.get('#moreInfo').click()
+    cy.contains("Steps To Use")
+    cy.get('iframe#howToVideo')
+    cy.get('iframe#howToVideo').should('have.attr', 'src')
+      .and('include', 'https://www.youtube.com/embed/W3Q5h756igs');
+    cy.get("#howToSignUpButton").click()
+    cy.url().should('eq', 'http://localhost:3000/signup');
+    
+    //cy.go('back')
+  })
+
+  it('Test multiple "Claims", check if link to signup page', function() {
+    cy.contains('Gomgom').parent().find('button').as('gomGomButton')
+    cy.get('@gomGomButton').click()
+    cy.url().should('eq', 'http://localhost:3000/signup');
+    cy.go('back')
+    cy.contains('Eltelierworks').parent().find('button').as('eltelierWorksButton')
+    cy.get('@eltelierWorksButton').click()
+    cy.url().should('eq', 'http://localhost:3000/signup');
+  })
+
+  it('Testing more information when promotion is clicked', function() {
+    cy.get('[alt="Eltelierworks logo"]').click()
+    cy.go('back')
+    cy.contains('Eltelierworks').click()
+    cy.contains('Release date')
+    cy.contains('Expiry date')
+    cy.contains('About the business').click()
+    cy.contains('Browse products')
+    cy.contains('Terms & Conditions').click()
+    cy.contains('Mandatory Legal Information')
+    cy.get('#bigPromoClaimButton').click()
+    cy.url().should('eq', 'http://localhost:3000/signup')
+    cy.go('back')
+    cy.go('back')
+
+    cy.get('[alt="Gomgom logo"]').click()
+    cy.go('back')
+    cy.contains('Gomgom').click()
+    cy.contains('Release date')
+    cy.contains('Expiry date')
+    cy.contains('About the business').click()
+    cy.contains('Browse products')
+    cy.contains('Terms & Conditions').click()
+    cy.contains('Mandatory Legal Information')
+    cy.get('#bigPromoClaimButton').click()
+    cy.url().should('eq', 'http://localhost:3000/signup')
+  })
+
+  it.only('fuzzer testing for logging in', function() {
+    const iter = 0
+
+    function randomizeUser() {
+      const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+      let user = '';
+      const userLength = Math.floor(Math.random() * 10) + 5; // Random user length between 5 and 14 characters
+      for (let i = 0; i < userLength; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        user += chars.charAt(randomIndex);
+      }
+      return user;
+    }
+    
+    function randomizePassword() {
+      const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
+      let password = '';
+      const passLength = Math.floor(Math.random() * 15) + 8; // Random password length between 8 and 22 characters
+      for (let i = 0; i < passLength; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars.charAt(randomIndex);
+      }
+      return password;
+    }
+    
+    cy.contains('Login').click()
+    while (iter <10) {
+      const randomUser = randomizeUser()
+      const randomPass =  randomizePassword()
+      console.log('randomUser: ', randomUser)
+      console.log('randomPass: ', randomPass)
+    
+      cy.get('#emailLogin').type(randomUser)
+      cy.get('#passwordLogin').type(randomPass)
+      cy.get('#logginInButton').click()
+      cy.url().should('eq', 'http://localhost:3000/login')
+    }
+  })
+
+  //Testing for signing up
+  it('Signing Up', function() {
+    cy.contains('Sign Up').click()
+    cy.get('#name').type('tester')
+    cy.get('#email').type('tester@email.com')
+    cy.get('#password').type('helloworld')
+    cy.get('#confirmPassword').type('helloworld')
+    cy.get('#signUp').click()
+    cy.contains('Confirm Your Email')
+    cy.contains('Verify email later',{timeout:10000}).click()
+
+    cy.get('#hamburgerIcon').click()
+    cy.contains('Profile').click()
+    cy.contains('Logout').click()
+
+    cy.contains('Login').click()
+    cy.get('#emailLogin').type('tester@email.com')
+    cy.get('#passwordLogin').type('helloworld')
+    cy.get('#logginInButton').click()
+    
+    //Find side bar, log out, then i have to log out, then test log in.
+    
+  })
+
+
+  
+ //Test for logging in
+ /*
+ it.only('Login Test', function() {
+  cy.contains('Login').click()
+  cy.get('#emailLogin').type('tester@email.com')
+  cy.get('#passwordLogin').type('helloworld')
+  cy.get('#loginButton').click()
+ })
+ */
+})
+
+describe('After logging in', function() {
+
+  const login = () => {
+  cy.visit('http://localhost:3000')
+
+  // Perform the login steps
+  cy.contains('Login').click()
+  cy.get('#emailLogin').type('tester@email.com')
+  cy.get('#passwordLogin').type('helloworld')
+  cy.get('#logginInButton').click()
+  cy.wait(1000) // Wait for any necessary network requests or animations
+
+  // Set a flag in Cypress to indicate that the login has been performed
+  Cypress.env('loggedIn', true)
+}
+
+  beforeEach(function() {
+    cy.visit('http://localhost:3000')
+
+    if (!Cypress.env('loggedIn')) {
+      login()
+    }
+
+    cy.visit('http://localhost:3000')
+    cy.get('#hamburgerIcon')
+  })
+
+    it('Front Page and Footer Information', function() {
+      cy.contains('Claim Promos')
+      cy.contains('Gomgom')
+      cy.contains('Promotions Available')
+      cy.contains('Contact Us')
+      cy.contains('Telegram: @alestierK')
+      cy.contains('Claims available')
+      cy.contains('Claim promotions from your favourite brands now!')
+    
+    })
+
+    it('Question Mark', function() {
+      cy.get('#questionMark').click()
+      cy.contains('How does PinchPromo work?')
+      cy.contains('Happy Pinching Promos')
+      cy.contains('Close').click()
+    })
+
+    it('Testing more information when promotion is clicked', function() {
+          cy.get('[alt="Eltelierworks logo"]').click()
+          cy.go('back')
+          cy.contains('Eltelierworks').click()
+          cy.contains('Release date')
+          cy.contains('Expiry date')
+          cy.contains('About the business').click()
+          cy.contains('Browse products')
+          cy.contains('Terms & Conditions').click()
+          cy.contains('Mandatory Legal Information')
+          //cy.get('#bigPromoClaimButton').click()
+          //cy.url().should('eq', 'http://localhost:3000/signup')
+          cy.go('back')
+      
+          cy.get('[alt="Gomgom logo"]').click()
+          cy.go('back')
+          cy.contains('Gomgom').click()
+          cy.contains('Release date')
+          cy.contains('Expiry date')
+          cy.contains('About the business').click()
+          cy.contains('Browse products')
+          cy.contains('Terms & Conditions').click()
+          cy.contains('Mandatory Legal Information')
+          cy.go('back')
+          //cy.get('#bigPromoClaimButton').click()
+          //cy.url().should('eq', 'http://localhost:3000/signup')
+      
+    })
+
+    it('[HAMBURGER] Update Password', function() {
+      //Update Password
+      cy.get('#hamburgerIcon').click()
+      cy.get('#hamburgerProfileLink').click()
+      cy.get('#updateEmailInput').should('have.value', 'tester@email.com')
+      cy.get('#updatePasswordInput').type('rightpassword')
+      cy.get('#updateConfirmPasswordInput').type('testwrongpassword2')
+      cy.get('#updateButton').click()
+      cy.contains('Passwords do not match')
+      cy.get('#updateConfirmPasswordInput').clear()
+      cy.get('#updateConfirmPasswordInput').type('rightpassword')
+      cy.get('#updateButton').click()
+      cy.wait(2000)
+
+      cy.get('#hamburgerIcon').click()
+      cy.get('#hamburgerProfileLink').click()
+      cy.get('#logoutButton').click()
+      cy.url().should('eq', 'http://localhost:3000/login')
+      cy.get('#emailLogin').type('tester@email.com')
+      cy.get('#passwordLogin').type('rightpassword')
+      cy.get('#logginInButton').click()
+
+      //Update Email
+      cy.get('#hamburgerIcon').click()
+      cy.contains('Profile').click()
+      cy.get('#updateEmailInput').should('have.value', 'tester@email.com')
+      cy.get('#updateEmailInput').clear()
+      cy.get('#updateEmailInput').type('updatedTester@email.com')
+      cy.get('#updateButton').click()
+      cy.wait(2000)
+
+      cy.get('#hamburgerIcon').click()
+      cy.get('#hamburgerProfileLink').click()
+      cy.get('#logoutButton').click()
+      cy.url().should('eq', 'http://localhost:3000/login')
+      cy.get('#emailLogin').type('updatedTester@email.com')
+      cy.get('#passwordLogin').type('rightpassword')
+      cy.get('#logginInButton').click()
+
+      cy.get('#hamburgerIcon').click()
+      cy.get('#hamburgerProfileLink').click()
+      /*
+      Fails due to no capital letters setting, please fix
+      cy.get('#updateEmailInput').should('have.value', 'updatedTester@email.com')  
+      */
+
+    })
+
+    it('[HAMBURGER] How to Use', function() {
+      cy.get('#hamburgerIcon').click()
+      cy.contains('How to use').click()
+      cy.url().should('eq', 'http://localhost:3000/howtouse')
+      cy.contains('Claim New Promos').click()
+    })
+
+    // it('[HAMBURGER]  Receipt Upload', function() {
+
+    // })
+    
+    it('[HAMBURGER] Claim', function() {
+      cy.contains('Gomgom').parent().find('button').as('gomGomButton')
+      cy.get('@gomGomButton').click()
+      cy.url().should('eq', 'http://localhost:3000/mypromotions')
+      cy.go('back')  
+      cy.get('#hamburgerIcon').click()
+      cy.get('#hamburgerClaimedLink').click()
+      cy.url().should('eq', 'http://localhost:3000/mypromotions')
+      cy.get('[alt="Gomgom logo"]').click()
+      cy.contains('[Important] How to use').click()
+      cy.contains('About Business').click()
+      cy.contains('Terms & Conditions').click()
+      cy.contains('Mandatory Legal Information')
+      cy.get('#claimingButton').click()
+      cy.contains('Note: Before Using Coupon')
+      cy.get('#cancelButton').click()
+      cy.get('#claimingButton').click()
+      cy.get('#confirmUseButton').click()
+      cy.contains('Time remaining:')
+      cy.contains('Promocode:')
+      // cy.contains('Ready to Use Promos')
+    })
+
+    it('Used Promos are stored', function() {
+      cy.get('#hamburgerIcon').click()
+      cy.get('#hamburgerClaimedLink').click()
+      cy.contains('Used Promos').click()
+      cy.contains('Gomgom')
+
+    })
+
+    it("Claiming before claim is reset", function() {
+      cy.contains('0/1')
+      cy.contains('New claim in ')
+      cy.contains('Eltelierworks').parent().find('button').as('eltelierWorksbutton')
+      cy.get('@eltelierWorksbutton').click()
+      cy.contains('Wait till your claim recharges to claim another promotion.')
+    })
+
+
+  })
+
+
+// Test flow: claim from promos page, click on logo, use the claim,check if speech bubble pops up, use claim, check for pinch promo X seomthign
+//Test functionalities when logged in. 1: claims,  receipt claim
+// Test if claims available will be deducted or not
