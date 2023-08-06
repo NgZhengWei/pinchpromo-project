f4597a35e020971250c71dc5d4e95ff1a6d1b3bd
NgZhengWei <ploxybee@gmail.com>
2023-08-06 21:45:56 +0800

    added test case for profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -11,44 +11,48 @@ import {
   FormLabel,
   Input,
   useToast,
-} from '@chakra-ui/react';
-import React, { useRef, useState } from 'react';
-import { Form } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
-import { useNavigate } from 'react-router-dom';
-import { CheckIcon, UnlockIcon } from '@chakra-ui/icons';
+} from "@chakra-ui/react";
+import React, { useEffect, useRef, useState } from "react";
+import { Form } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { CheckIcon, UnlockIcon } from "@chakra-ui/icons";
 
 const Profile = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
   const confirmPasswordRef = useRef();
   const { currentUser, updateEmail, updatePassword, logout } = useAuth();
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const toast = useToast();
+
+  useEffect(() => {
+    document.title = "Profile";
+  }, []);
 
   async function handleLogout(e) {
     try {
       await logout();
       toast({
-        title: 'Logged Out',
-        description: 'Successfully logged out',
+        title: "Logged Out",
+        description: "Successfully logged out",
         isClosable: true,
         duration: 3000,
-        status: 'success',
-        position: 'top',
+        status: "success",
+        position: "top",
         icon: <UnlockIcon />,
       });
-      navigate('/login');
+      navigate("/login");
     } catch {
       toast({
-        title: 'Logged Out',
-        description: 'Failed to logged out',
+        title: "Logged Out",
+        description: "Failed to logged out",
         isClosable: true,
         duration: 3000,
-        status: 'error',
-        position: 'bottom',
+        status: "error",
+        position: "bottom",
         icon: <UnlockIcon />,
       });
     }
@@ -57,12 +61,12 @@ const Profile = () => {
   function handleSumit(e) {
     e.preventDefault();
     if (passwordRef.current.value !== confirmPasswordRef.current.value) {
-      return setError('Passwords do not match');
+      return setError("Passwords do not match");
     }
 
     const promises = [];
     setLoading(true);
-    setError('');
+    setError("");
     if (emailRef.current.value !== currentUser.email) {
       promises.push(updateEmail(emailRef.current.value));
     }
@@ -71,19 +75,19 @@ const Profile = () => {
     }
     Promise.all(promises)
       .then(() => {
-        navigate('/');
+        navigate("/");
         toast({
-          title: 'Profile successfully updated.',
+          title: "Profile successfully updated.",
           isClosable: true,
           duration: 3000,
-          status: 'success',
-          position: 'top',
+          status: "success",
+          position: "top",
           icon: <CheckIcon />,
         });
       })
       .catch((e) => {
-        if (e.code === 'auth/requires-recent-login') {
-          setError('Please logout and login again to reset your password.');
+        if (e.code === "auth/requires-recent-login") {
+          setError("Please logout and login again to reset your password.");
         }
         setError(e.message);
       })
@@ -94,28 +98,28 @@ const Profile = () => {
 
   return (
     <Box
-      display='flex'
-      w='90%'
-      mx='auto'
-      maxW='800px'
-      flexDir='column'
-      alignItems='center'
+      display="flex"
+      w="90%"
+      mx="auto"
+      maxW="800px"
+      flexDir="column"
+      alignItems="center"
     >
-      <Card w='100%' mb='20px'>
+      <Card w="100%" mb="20px">
         <CardHeader
-          fontFamily='Arial Rounded MT Bold'
-          textAlign='center'
-          fontSize='30px'
+          fontFamily="Arial Rounded MT Bold"
+          textAlign="center"
+          fontSize="30px"
         >
           Update Profile
         </CardHeader>
         <CardBody>
           {error && (
             <Alert
-              status='error'
-              variant='left-accent'
+              status="error"
+              variant="left-accent"
               borderRadius={4}
-              mb='10px'
+              mb="10px"
             >
               <AlertIcon />
               <AlertTitle>{error}</AlertTitle>
@@ -123,41 +127,41 @@ const Profile = () => {
           )}
 
           <Form onSubmit={handleSumit}>
-            <FormControl mb='20px'>
+            <FormControl mb="20px">
               <FormLabel>Email</FormLabel>
               <Input
-                type='email'
-                name='email'
+                type="email"
+                name="email"
                 ref={emailRef}
                 required
                 defaultValue={currentUser.email}
               />
             </FormControl>
-            <FormControl mb='20px'>
+            <FormControl mb="20px">
               <FormLabel>New Password</FormLabel>
               <Input
-                type='password'
-                name='password'
+                type="password"
+                name="password"
                 ref={passwordRef}
-                placeholder='Leave blank to keep the same'
+                placeholder="Leave blank to keep the same"
               />
             </FormControl>
-            <FormControl mb='20px'>
+            <FormControl mb="20px">
               <FormLabel>New Confirmed Password</FormLabel>
               <Input
-                type='password'
-                name='confirm-password'
+                type="password"
+                name="confirm-password"
                 ref={confirmPasswordRef}
-                placeholder='Leave blank to keep the same'
+                placeholder="Leave blank to keep the same"
               />
             </FormControl>
-            <Button disabled={loading} type='submit' w='100%' colorScheme='red'>
+            <Button disabled={loading} type="submit" w="100%" colorScheme="red">
               Update
             </Button>
           </Form>
         </CardBody>
       </Card>
-      <Button onClick={handleLogout} variant='ghost' colorScheme='gray'>
+      <Button onClick={handleLogout} variant="ghost" colorScheme="gray">
         Logout
       </Button>
       {/* <Text>
